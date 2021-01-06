543e69b49d4a0f5b43520cb51d8326172c8a4b30
smilyk <smilyk1982@gmail.com>
2021-01-06 19:45:33 +0200

    bug with driver chrome

--- src/main/java/atsaratbriut/school/service/parser/ParsServiceImpl.java
+++ src/main/java/atsaratbriut/school/service/parser/ParsServiceImpl.java
@@ -8,7 +8,6 @@ import atsaratbriut.school.service.email.EmailService;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
@@ -135,13 +134,7 @@ public class ParsServiceImpl implements ParsService {
     }
 
     private WebDriver getWebDriver() {
-
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("window-size=1200x600");
-
-        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver();
         driver.get(schoolUrl);
         return driver;
     }
